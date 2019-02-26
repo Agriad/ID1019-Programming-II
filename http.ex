24dd297018681f8661e263ee47332a3552b6724e
Agriad <agriad1@yahoo.com>
2019-02-26 13:51:28 +0100

    20.0

--- http.ex
+++ http.ex
@@ -44,4 +44,96 @@ defmodule HTTP do
     end
 
     def message_body(r), do: {r, []}
+
+    def ok(body) do
+        "HTTP/1.1 200 OK\r\n\r\n #{body}"
+    end
+
+    def get(uri) do
+        "GET #{uri} HTTP/1.1\r\n\r\n"
+    end
+end
+
+defmodule Rudy do
+
+    def init(port) do
+        opt = [:list, active: false, reuseaddr: true]
+
+        case :gen_tcp.listen(port, opt) do
+            {:ok, listen} ->
+                handler(listen)
+                :gen_tcp.close(listen)
+                :ok
+            {:error, error} ->
+                error
+        end
+    end
+
+    def handler(listen) do
+        case :gen_tcp.accept(listen) do
+            {:ok, client} ->
+                request(client)
+            {:error, error} ->
+            error
+        end
+    end
+
+    def request(client) do
+        recv = :gen_tcp.recv(client, 0)
+        case recv do
+            {:ok, str} ->
+                answer = HTTP.parse_request(str) 
+                response = reply(answer)
+                :gen_tcp.send(client, response)
+            {:error, error} ->
+                IO.puts("RUDY ERROR: #{error}")
+        end
+        :gen_tcp.close(client)
+    end
+
+    def reply({{:get, uri, _}, _, _}) do
+        HTTP.ok("Hello!")
+    end
+
+    # def reply({{:get, uri, _}, _, _}) do
+    #     :timer.sleep(10)
+    #     HTTP.ok("Hello!")
+    # end
+
+    def start(port) do
+        Process.register(spawn(fn -> init(port) end), :rudy)
+    end
+
+    def stop() do
+        Process.exit(Process.whereis(:rudy), "Time to die!")
+    end
+
+end
+
+defmodule Test do
+
+    @number_requests 100
+
+    def bench(host, port) do
+        start = Time.utc_now()
+        run(@number_requests, host, port)
+        finish = Time.utc_now()
+        diff = Time.diff(finish, start, :millisecond)
+        IO.puts("Benchmark: #{@number_requests} requests in #{diff} ms")
+    end
+
+    defp run(0, _host, _port), do: :ok
+    defp run(n, host, port) do
+        request(host, port)
+        run(n - 1, host, port)
+    end
+
+    defp request(host, port) do
+        opt = [:list, active: false, reuseaddr: true]
+        {:ok, server} = :gen_tcp.connect(host, port, opt)
+        :gen_tcp.send(server, HTTP.get("foo"))
+        {:ok, _reply} = :gen_tcp.recv(server, 0)
+        :gen_tcp.close(server)
+    end
+
 end
